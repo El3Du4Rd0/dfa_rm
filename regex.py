c9de46a2d030913fefbf11db89091119ca94bfa8
El3Du4Rd0 <111295855+El3Du4Rd0@users.noreply.github.com>
2024-06-05 01:25:56 -0600

    Add files via upload

--- regex.py
+++ regex.py
@@ -1,19 +1,45 @@
 import re
 
 def parse(string):
-    pattern = r'(\brm-f\b)|(\brm-i\b)|(\brm-r\b)|(\brm-d\b)|(\brm-v\b)|(\brm--preserve-root\b)|(\brm--no-preserve-root\b)|(\brm--one-file-system\b)|(\brm--help\b)|(\brm--version\b)'
+    """
+    Check if the given string is a valid command using a regular expression.
+    
+    Args:
+        string (str): The command string to be checked.
+        
+    Prints:
+        A message indicating whether the string is a valid command.
+    """
+    pattern = (
+        r'(\brm-f\b)|(\brm-i\b)|(\brm-r\b)|(\brm-d\b)|(\brm-v\b)|'
+        r'(\brm--preserve-root\b)|(\brm--no-preserve-root\b)|'
+        r'(\brm--one-file-system\b)|(\brm--help\b)|(\brm--version\b)'
+    )
     match = re.match(pattern, string)
     if match:
         print(f"'{string}' is a valid command")
     else:
         print(f"'{string}' is an invalid command")
 
+# List of valid commands
+valid_commands = [
+    "rm-f", "rm-i", "rm-r", "rm-d", "rm-v",
+    "rm--preserve-root", "rm--no-preserve-root", 
+    "rm--one-file-system", "rm--help", "rm--version"
+]
 
-valid_commands = ["rm-f","rm-i","rm-r","rm-d","rm-v","rm--preserve-root","rm--no-preserve-root","rm--one-file-system","rm--help","rm--version"]
-invalid_commands = ["rm-","rm-file","rm-file-system","rm-heap","rm-vvvv","tamales","huevo","frijoles","tortillas","enchiladas","rm--preserve","rm--one","rm--no","rm--pleh","rm--sionver"]
+# List of invalid commands
+invalid_commands = [
+    "rm-", "rm-file", "rm-file-system", "rm-heap", 
+    "rm-vvvv", "tamales", "huevo", "frijoles", 
+    "tortillas", "enchiladas", "rm--preserve", 
+    "rm--one", "rm--no", "rm--pleh", "rm--sionver"
+]
 
+# Check each valid command
 for command in valid_commands:
     parse(command)
 
+# Check each invalid command
 for command in invalid_commands:
     parse(command)
